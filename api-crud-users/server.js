c63672433a6bd2e57637fd013d1c0a57f7be389b
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-11-20 18:39:11 -0300

    feat: create request find user by name

--- api-crud-users/server.js
+++ api-crud-users/server.js
@@ -17,7 +17,18 @@ app.post("/users", async (req, res) => {
 })
 
 app.get("/users", async (req, res) => {
-    const users = await prisma.user.findMany()
+
+    let users = [];
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+    }else{
+        users = await prisma.user.findMany()
+    }
     res.status(200).json(users)
 })
 
