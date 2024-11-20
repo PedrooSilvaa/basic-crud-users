a0ba0877aa1aa349a117a485319dc303cd7a3ae2
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-11-20 18:20:52 -0300

    feat: database connection and install prisma

--- api-crud-users/server.js
+++ api-crud-users/server.js
@@ -1,17 +1,24 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 const app = express();
 app.use(express.json())
 
-const users = [];
-
-app.post("/users", (req, res) => {
-    users.push(req.body)
-    res.send("Ok post") 
+app.post("/users", async (req, res) => {
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+    res.status(201).send(req.body) 
 })
 
-app.get("/users", (req, res) => {
-    res.json(users)
+app.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.status(200).json(users)
 })
 
 app.listen(3000)
