3ad9cfe7406b5110fff495236689f079ff066e7e
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-11-20 18:28:00 -0300

    feat: create request put update user

--- api-crud-users/server.js
+++ api-crud-users/server.js
@@ -21,4 +21,18 @@ app.get("/users", async (req, res) => {
     res.status(200).json(users)
 })
 
+app.put("/users/:id", async (req, res) => {
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name, 
+            age: req.body.age,
+        }
+    })
+    res.status(201).send(req.body) 
+})
+
 app.listen(3000)
